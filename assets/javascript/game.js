e3df2e52abeb22f30c6dde9326ce3eb8b15c5b88
Jennifer Zimmerman <jennifer.faye0101@gmail.com>
2019-07-22 17:45:19 -0500

    continuing to refine game logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var gameStart= false;
+var gameStart = false;
 var gameOver = false;
 var resetPlay;
 var wins = 0;
@@ -6,14 +6,24 @@ var losses = 0;
 var yourTotal = 0;
 
 
-function gamestart() {
-    console.log("game is starting")
+
+//start game
+function gameStart() {
     gameStart = false;
     wins = 0;
     losses = 0;
     yourtotal = 0;
-    randomNumber(); 
-}  $("#winscore").html("0");
+} console.log("game is starting")
+
+
+
+
+//shows 0 for score on startup
+$("#winscore").text("0");
+$("#losescore").text("0");
+$("#totalscore").text("0");
+
+
 
 
 //generate random number between 19-120
@@ -21,23 +31,34 @@ var lowNumber = 19;
 var highNumber = 120;
 var randomNumber = numberRange(lowNumber, highNumber);
 
-    function numberRange(min,max)
+function numberRange(min,max)
 {
     return Math.floor(Math.random()*(max-min+1)+min);
 }  $("#generated").html(randomNumber);
 
-
-
-
-
-//$("#generated").text(numberRange);
-
-//$("#generated").html(numberRange);
 console.log(randomNumber);
-/*#crystal1
-    #cyrstal2
-    #cyrstal3
-    #cyrstal4*/
+
+
+
+
+//genrates random number between 1-12 for each crystal
+//function assignGemRandom () {
+$("#crystal1").attr("value", Math.floor(Math.random() * 12) + 1);
+$("#crystal2").attr("value", Math.floor(Math.random() * 12) + 1);
+$("#crystal3").attr("value", Math.floor(Math.random() * 12) + 1);
+$("#crystal4").attr("value", Math.floor(Math.random() * 12) + 1);
+//}
+console.log(crystal1, crystal2, crystal3, crystal4)
+
+
+
+
+// $("#crystal1").attr("#buttons");
+// var yourTotal = $( "#totalScore" )
+// $("#crystal1").click(function () {     
+//      buttons += yourTotal;
+//        //#update the display of score on the page
+// }); console.log(yourTotal)
 
 
 
@@ -45,3 +66,12 @@ console.log(randomNumber);
 
 
 
+
+
+
+
+  //wins:
+        //$(".button-title-chosen-for-first-button").click(function (){
+                //score += crystalValue1;
+                //#update the display of the score on the page
+            //});
