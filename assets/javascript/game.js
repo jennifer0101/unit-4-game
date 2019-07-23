bd8c0b4cb140f420458857b9d027cbfe89807ba8
Jennifer Zimmerman <jennifer.faye0101@gmail.com>
2019-07-22 23:31:56 -0500

    game is now functioning

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,28 +9,12 @@ var highNumber = 120;
 var randomNumber = 0;
 
 
-    // $("#winscore").html(wins);
-    // $("#losescore").html(losses);
-    // $("#totalscore").html(yourTotal);
-    // $("#generated").html(randomNumber);
-    // $("#crystal1").attr("value", Math.floor(Math.random() * 12) + 1);
-    // $("#crystal2").attr("value", Math.floor(Math.random() * 12) + 1);
-    // $("#crystal3").attr("value", Math.floor(Math.random() * 12) + 1);
-    // $("#crystal4").attr("value", Math.floor(Math.random() * 12) + 1);
-    // console.log(crystal1, crystal2, crystal3, crystal4)
-
-//generate random number between 19-120
-// function numberRange(min,max){
-//     return Math.floor(Math.random()*(max-min+1)+min);
-// }  
-
-
-
 $("#crystal1").on("click", function() {     
     var t=$(this)
     var value = parseInt(t[0].attributes[1].value)
     yourTotal += value;
-    //console.log(yourTotal)
+    $("#totalscore").html(yourTotal);
+    console.log(yourTotal);
     checkRandomNumber();
 });
 
@@ -38,7 +22,8 @@ $("#crystal2").on("click", function() {
     var t=$(this)
     var value = parseInt(t[0].attributes[1].value)
     yourTotal += value;
-    //console.log(yourTotal)
+    $("#totalscore").html(yourTotal);
+    console.log(yourTotal);
     checkRandomNumber();
 });
 
@@ -46,7 +31,8 @@ $("#crystal3").on("click", function() {
     var t=$(this)
     var value = parseInt(t[0].attributes[1].value)
     yourTotal += value;
-    //console.log(yourTotal)
+    $("#totalscore").html(yourTotal);
+    console.log(yourTotal);
     checkRandomNumber();
 });
 
@@ -54,19 +40,23 @@ $("#crystal4").on("click", function() {
     var t=$(this)
     var value = parseInt(t[0].attributes[1].value)
     yourTotal += value;
-    //console.log(yourTotal)
+    $("#totalscore").html(yourTotal);
+    console.log(yourTotal);
     checkRandomNumber();
 });
 
 function checkRandomNumber(){
-    console.log(yourTotal)
+    //console.log(yourTotal)
     if (yourTotal===randomNumber) {
         wins++;
         $("#winscore").html(wins);
-        alert("Won Game");
+        //alert("Won Game");
         startGame();
     } else if (yourTotal>randomNumber) {
-        console.log("lose")
+        losses++;
+        $("#losescore").html(losses);
+        //alert("Lost Game");
+        startGame();
     }
 } 
 
@@ -83,10 +73,6 @@ function startGame() {
     $("#crystal2").attr("value", Math.floor(Math.random() * 12) + 1);
     $("#crystal3").attr("value", Math.floor(Math.random() * 12) + 1);
     $("#crystal4").attr("value", Math.floor(Math.random() * 12) + 1);
+    console.log("reset")
 } 
 startGame();
-  //wins:
-        //$(".button-title-chosen-for-first-button").click(function (){
-                //score += crystalValue1;
-                //#update the display of the score on the page
-            //});
