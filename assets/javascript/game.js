2cfe556bd554a439e7dec721736e621c94caccbc
Jennifer Zimmerman <jennifer.faye0101@gmail.com>
2019-07-22 22:43:33 -0500

    worked more on js game, functioning wins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,75 +1,90 @@
-var gameStart = false;
-var gameOver = false;
+var gameStart;
+var gameOver;
 var resetPlay;
 var wins = 0;
 var losses = 0;
 var yourTotal = 0;
-
-
-
-//start game
-function gameStart() {
-    gameStart = false;
-    wins = 0;
-    losses = 0;
-    yourtotal = 0;
-} console.log("game is starting")
-
-
-
-
-//shows 0 for score on startup
-$("#winscore").text("0");
-$("#losescore").text("0");
-$("#totalscore").text("0");
-
-
-
-
-//generate random number between 19-120
 var lowNumber = 19;
 var highNumber = 120;
-var randomNumber = numberRange(lowNumber, highNumber);
-
-function numberRange(min,max)
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
-}  $("#generated").html(randomNumber);
-
-console.log(randomNumber);
+var randomNumber = 0;
 
 
+    // $("#winscore").html(wins);
+    // $("#losescore").html(losses);
+    // $("#totalscore").html(yourTotal);
+    // $("#generated").html(randomNumber);
+    // $("#crystal1").attr("value", Math.floor(Math.random() * 12) + 1);
+    // $("#crystal2").attr("value", Math.floor(Math.random() * 12) + 1);
+    // $("#crystal3").attr("value", Math.floor(Math.random() * 12) + 1);
+    // $("#crystal4").attr("value", Math.floor(Math.random() * 12) + 1);
+    // console.log(crystal1, crystal2, crystal3, crystal4)
 
-
-//genrates random number between 1-12 for each crystal
-//function assignGemRandom () {
-$("#crystal1").attr("value", Math.floor(Math.random() * 12) + 1);
-$("#crystal2").attr("value", Math.floor(Math.random() * 12) + 1);
-$("#crystal3").attr("value", Math.floor(Math.random() * 12) + 1);
-$("#crystal4").attr("value", Math.floor(Math.random() * 12) + 1);
-//}
-console.log(crystal1, crystal2, crystal3, crystal4)
+//generate random number between 19-120
+// function numberRange(min,max){
+//     return Math.floor(Math.random()*(max-min+1)+min);
+// }  
 
 
 
+$("#crystal1").on("click", function() {     
+    var t=$(this)
+    var value = parseInt(t[0].attributes[1].value)
+    yourTotal += value;
+    //console.log(yourTotal)
+    checkRandomNumber();
+});
 
-// $("#crystal1").attr("#buttons");
-// var yourTotal = $( "#totalScore" )
-// $("#crystal1").click(function () {     
-//      buttons += yourTotal;
-//        //#update the display of score on the page
-// }); console.log(yourTotal)
+$("#crystal2").on("click", function() {     
+    var t=$(this)
+    var value = parseInt(t[0].attributes[1].value)
+    yourTotal += value;
+    //console.log(yourTotal)
+    checkRandomNumber();
+});
+
+$("#crystal3").on("click", function() {     
+    var t=$(this)
+    var value = parseInt(t[0].attributes[1].value)
+    yourTotal += value;
+    //console.log(yourTotal)
+    checkRandomNumber();
+});
+
+$("#crystal4").on("click", function() {     
+    var t=$(this)
+    var value = parseInt(t[0].attributes[1].value)
+    yourTotal += value;
+    //console.log(yourTotal)
+    checkRandomNumber();
+});
+
+function checkRandomNumber(){
+    console.log(yourTotal)
+    if (yourTotal===randomNumber) {
+        wins++;
+        $("#winscore").html(wins);
+        alert("Won Game");
+        startGame();
+    } else if (yourTotal>randomNumber) {
+        console.log("lose")
+    }
+} 
 
 
 
-
-
-
-
-
-
-
-
+function startGame() {
+    randomNumber=Math.floor(Math.random()*(120-19+1)+19);
+    yourTotal=0;
+    $("#winscore").html(wins);
+    $("#losescore").html(losses);
+    $("#totalscore").html(yourTotal);
+    $("#generated").html(randomNumber);
+    $("#crystal1").attr("value", Math.floor(Math.random() * 12) + 1);
+    $("#crystal2").attr("value", Math.floor(Math.random() * 12) + 1);
+    $("#crystal3").attr("value", Math.floor(Math.random() * 12) + 1);
+    $("#crystal4").attr("value", Math.floor(Math.random() * 12) + 1);
+} 
+startGame();
   //wins:
         //$(".button-title-chosen-for-first-button").click(function (){
                 //score += crystalValue1;
